47f54612ca0ade9ef479b832e10ab2e5306e4047
Bruno Werneck <brunovaulawerneck@gmail.com>
2016-07-18 13:57:56 +0000

    Refactoring notifications

--- views/notifications.blade.php
+++ views/notifications.blade.php
@@ -1,39 +1,24 @@
-@if ($message = Session::get('success'))
-<div class="row">
-    <div class="alert alert-success alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <strong>Success:</strong> {!! $message !!}
-    </div>
-</div>
-<?php Session::forget('success');?>
-@endif
+<?php
 
-@if ($message = Session::get('error'))
-<div class="row">
-    <div class="alert alert-danger alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <strong>Error:</strong> {!! $message !!}
-    </div>
-</div>
-<?php Session::forget('error'); ?>
-@endif
+    $vars = Session::all();
+    foreach ($vars as $key => $value) {
+        switch($key) {
+            case 'success':
+            case 'error':
+            case 'warning':
+            case 'info':
+                ?>
+                <div class="row">
+                    <div class="alert alert-{{ $key }} alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <strong>{{ ucfirst($key) }}:</strong> {!! $value !!}
+                    </div>
+                </div>
+                <?php
+                Session::forget($key);
+                break;
+            default:
+        }
+    }
 
-@if ($message = Session::get('warning'))
-<div class="row">
-    <div class="alert alert-warning alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <strong>Warning:</strong> {!! $message !!}
-    </div>
-</div>
-<?php Session::forget('warning'); ?>
-@endif
-
-@if ($message = Session::get('info'))
-<div class="row">
-    <div class="alert alert-info alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <strong>FYI:</strong> {!! $message !!}
-    </div>
-</div>
-<?php Session::forget('info'); ?>
-@endif
+?>
